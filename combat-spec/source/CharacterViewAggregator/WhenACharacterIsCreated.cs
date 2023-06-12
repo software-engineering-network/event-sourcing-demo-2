b55a696442744ff5f6f577a879f1019f6a579ff8
Jarrod Swords <jarrod.swords@gmail.com>
2023-06-11 23:03:24 -0400

    idempotence check; shouldn't be right

--- combat-spec/source/CharacterViewAggregator/WhenACharacterIsCreated.cs
+++ combat-spec/source/CharacterViewAggregator/WhenACharacterIsCreated.cs
@@ -27,7 +27,6 @@ namespace EventSourcingDemo.CombatSpec.CharacterViewAggregator
         public void ThenCharacterIsAddedToView()
         {
             CharacterCreated @event;
-            ;
 
             _aggregator.Handle(@event = new CharacterCreated("Mario"));
 
@@ -38,5 +37,40 @@ namespace EventSourcingDemo.CombatSpec.CharacterViewAggregator
         }
 
         #endregion
+
+        public class GivenTheEventIsProcessed
+        {
+            #region Setup
+
+            private readonly Aggregator _aggregator;
+            private readonly MockEventStore _store = new();
+            private readonly MockViewRepository _viewRepository = new();
+
+            public GivenTheEventIsProcessed()
+            {
+                _aggregator = new Aggregator(_store, _viewRepository);
+                _aggregator.Start();
+            }
+
+            #endregion
+
+            #region Requirements
+
+            [Fact]
+            public void ThenNoOp()
+            {
+                CharacterCreated @event;
+
+                _aggregator.Handle(@event = new CharacterCreated("Mario"));
+                _aggregator.Handle(@event);
+
+                var view = (CharacterView) _viewRepository.Find("CharacterView").Value;
+                var expected = new List<Character> { new(@event.EntityId, "Mario") };
+
+                view.Characters.Should().BeEquivalentTo(expected);
+            }
+
+            #endregion
+        }
     }
 }
