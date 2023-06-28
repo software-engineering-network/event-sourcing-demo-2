22566a5f1e50fe1901f8df252b95ba98fcc330d9
Jarrod Swords <jarrod.swords@gmail.com>
2023-06-28 09:21:19 -0400

    add test

--- combat-spec/source/items/WhenRegisteringAnItem.cs
+++ combat-spec/source/items/WhenRegisteringAnItem.cs
@@ -13,15 +13,17 @@ namespace EventSourcingDemo.CombatSpec.Items
 
         private readonly RegisterItem _command;
         private readonly ItemRegistered _event;
+        private readonly Result _result;
+        private readonly Event[] _stream;
 
         public WhenRegisteringAnItem()
         {
             var service = CreateService(out var store);
             service.Handle(_command = new RegisterItem("Mushroom"));
 
-            var stream = store.Find(new StreamId(ItemManager.Category, _command.EntityId)).Value;
+            _stream = store.Find(new StreamId(ItemManager.Category, _command.EntityId));
 
-            _event = (ItemRegistered) stream[0];
+            _event = (ItemRegistered) _stream[0];
         }
 
         #endregion
@@ -29,8 +31,16 @@ namespace EventSourcingDemo.CombatSpec.Items
         #region Requirements
 
         [Fact]
+        public void ThenAnEntityStreamIsCreated() => _stream.Should().HaveCount(1);
+
+        [Fact]
+        public void ThenAnItemWasRegistered() => _event.Should().BeOfType(typeof(ItemRegistered));
+
+        // todo: move to event test
+        [Fact]
         public void ThenCategoryIsItemRegistration() => _event.Category.Should().Be(ItemManager.Category);
 
+        // todo: move to event test
         [Fact]
         public void ThenEntityIdMatchesCommandEntityId() => _event.EntityId.Should().Be(_command.EntityId);
 
